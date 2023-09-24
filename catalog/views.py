267fb5abc04261b8783dc3643ff00460055bd231
aleksey <lhadkov@mail.ru>
2023-09-24 23:09:36 +0300

    update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home_page(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts_page(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"name:{name}, phone:{phone}, message:{message}")
+    return render(request, 'catalog/contacts.html')
