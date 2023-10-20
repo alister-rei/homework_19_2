505162099ac6e20a7fc9e6c5ab80668e331b320a
aleksey <lhadkov@mail.ru>
2023-10-20 16:54:02 +0300

    update catalog urls FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home_page, contacts_page, product
+from catalog.views import HomepageListView, ContactsPageView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home_page, name='home'),
-    path('contacts/', contacts_page, name='contacts'),
-    path('product/<int:pk>/', product, name='product'),
+    path('', HomepageListView.as_view(), name='home'),
+    path('contacts/', ContactsPageView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
 ]
