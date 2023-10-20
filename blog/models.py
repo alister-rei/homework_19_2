141e6a966888db415d8ff57762bb71b26c11ae00
aleksey <lhadkov@mail.ru>
2023-10-20 16:59:42 +0300

    add blog models Post

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    slug = models.CharField(max_length=100, **NULLABLE, verbose_name='Slug')
+    description = models.TextField(**NULLABLE, verbose_name='Содержимое')
+    image = models.ImageField(upload_to='product/', **NULLABLE, verbose_name='Изображение(превью)')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    views_count = models.IntegerField(default=0, verbose_name='просмотров')
+
+    def __str__(self):
+        return f'{self.title}, {self.slug}'
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Пост'
+
